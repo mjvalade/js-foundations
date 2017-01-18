c9196569dcb0909253a7dd927319f9d46123193e
mjvalade <mjvalade@gmail.com>
2017-01-18 15:56:27 -0700

    unicorn tests passing

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,3 +1,19 @@
-function Unicorn() {}
+function Unicorn(name, color) {
+  this.name = name;
+  this.color = color || 'white';
+}
+
+Unicorn.prototype.isWhite = () => {
+  if (this.color === "white") {
+    return;
+  } else {
+    return false;
+  }
+};
+
+Unicorn.prototype.says = function(stuff) {
+  return ("**;* " + stuff + " *;**");
+};
+
 
 module.exports = Unicorn;
