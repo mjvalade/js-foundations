6db9559dc7911526f22209c9b347564050772fab
mjvalade <mjvalade@gmail.com>
2017-02-14 20:56:51 -0700

    hobbit tests passing

--- mythical-creatures/exercises/hobbit.js
+++ mythical-creatures/exercises/hobbit.js
@@ -2,21 +2,20 @@ function Hobbit(name, disposition, age, adult, isShort, old){
   this.name = name;
   this.disposition = disposition || 'homebody';
   this.age = age || 0;
-  this.adult = () => {
-    if(this.age >= 33){
-      return true;
-    } else return false;
-  };
+  this.adult = false;
   this.isShort = true;
-  this.old = function(){
-    if(this.age >= 101){
-      return true;
-    } else return false;
-  };
+  this.old = false;
+  this.hasRing = this.name === 'Frodo' || false;
 }
 
 Hobbit.prototype.celebrateBirthday = function() {
   this.age++;
+  if (this.age >= 33) {
+    this.adult = true;
+  }
+  if (this.age > 100) {
+    this.old = true;
+  }
 };
 
 module.exports = Hobbit;
